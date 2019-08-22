9470b05daf93563dcc580cec5b48cd9cf272ca6d
nimosaid <saidnimo123@gmail.com>
2019-08-22 09:36:19 +0300

    writting code

--- pics/urls.py
+++ pics/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns=[
     url('^$',views.welcome,name = 'welcome'),
+     url('^today/$',views.pics_of_day,name='picsToday')
 ]
--- pics/views.py
+++ pics/views.py
@@ -1,5 +1,30 @@
 from django.http  import HttpResponse
+import datetime as dt
 
 # Create your views here.
 def welcome(request):
     return HttpResponse('The Photo Gallery')
+
+def pics_of_day(request):
+    date = dt.date.today()
+     # FUNCTION TO CONVERT DATE OBJECT TO FIND EXACT DAY
+    day = convert_dates(date)
+    html = f'''
+        <html>
+            <body>
+                <h1>Pics for {day} {date.day}-{date.month}-{date.year}</h1>
+            </body>
+        </html>
+            '''
+    return HttpResponse(html)
+
+def convert_dates(dates):
+
+    # Function that gets the weekday number for the date.
+    day_number = dt.date.weekday(dates)
+
+    days = ['Monday','Tuesday','Wednesday','Thursday','Friday','Saturday',"Sunday"]
+
+    # Returning the actual day of the week
+    day = days[day_number]
+    return day
