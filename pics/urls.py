02e7811d8d5070b47091e0414b855c7d5b713636
nimosaid <saidnimo123@gmail.com>
2019-08-23 11:19:05 +0300

    add changes

--- pics/urls.py
+++ pics/urls.py
@@ -3,6 +3,5 @@ from . import views
 
 urlpatterns=[
     url(r'^$',views.pics_of_day,name='picsToday'),
-    url(r'^archives/(\d{4}-\d{2}-\d{2})/$',views.past_days_pics,name = 'pastPics'),
     url(r'^search/', views.search_results, name='search_results')
 ]
